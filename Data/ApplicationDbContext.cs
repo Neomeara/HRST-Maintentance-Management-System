6d21c393cbaa11bfd7f7d602a4bca5d6e0eed23b
Nomeara <omeara.nate@gmail.com>
2021-12-19 22:16:07 -0600

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using HRST_Maintenance_Management_System.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace HRST_Maintenance_Management_System.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
