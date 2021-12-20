c56c3e9e9d4e5ed098d18ac5ffc4790dc54f4cdd
Nomeara <omeara.nate@gmail.com>
2021-12-19 23:30:30 -0600

    Rename to fix weird error

---
+++ Config.cs
@@ -0,0 +1,18 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace HRST_Maintenance_Management_System
+{
+    public class Config
+    {
+
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>
+    {
+        new ApiScope(name: "read",   displayName: "Read your data."),
+        new ApiScope(name: "write",  displayName: "Write your data."),
+        new ApiScope(name: "delete", displayName: "Delete your data.")
+    };
+        }
+    }
+}
